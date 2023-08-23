0534114779f424f368fb81a1cad477b6b7d304a0
Naveen921 <donthamnaveen@gmail.com>
2023-08-24 00:22:37 +0530

    Instillation

---
+++ 10-Functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#our goal is to insatll git
+
+VALIDATE(){
+    #$1-->it will recive the argument1 i.e. VALIDATE $?
+    #$2-->it will recive the argument2 i.e. VALIDATE ($?) $2"
+if [ $1 -ne 0 ]
+then
+    echo "$2 is.............. ERROR"
+    exit 1
+else
+    echo "$2 is.............. Success"
+fi    
+}
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root user"
+    exit 1
+fi
+
+yum install git -y
+VALIDATE $? "Insatlling git"
+
+yum install mysql -y
+VALIDATE $? "Installing mysql"
+
