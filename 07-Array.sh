6f52841ce282f66f8eaa9b402cc94a7c47d29bf2
Naveen921 <donthamnaveen@gmail.com>
2023-08-23 23:10:41 +0530

    variables

--- 07-Array.sh
+++ 07-Array.sh
@@ -3,4 +3,4 @@
 #this can hold multipole values instead of single value
 PERSONS=("Naveen""Venu""Puri")
 
-echo "The name of a first person is : $PERSON(0)"
+echo "The name of a first person is : ${PERSON[0]}"
