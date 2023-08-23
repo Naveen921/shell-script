356a48092bdac934cb00845f90f4ef9a57ec80a8
Naveen921 <donthamnaveen@gmail.com>
2023-08-24 02:05:50 +0530

    Multipleinstall-loops

--- 13-Mutipleinstall-Loops.sh
+++ 13-Mutipleinstall-Loops.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+USERID=(id -u)
+if [$USERID -ne 0]
+then
+    echo -e "$R ERROR:: Procced with ROOT access $N"
+    exit 1
+fi
+
 for i in  $@
 do
     yum install $i -y
