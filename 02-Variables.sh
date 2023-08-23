da7d0c23f5c743624384d5ee1f816e3d78e4fb12
Naveen921 <donthamnaveen@gmail.com>
2023-08-23 22:48:18 +0530

    variables

--- 02-Variables.sh
+++ 02-Variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Person1=$1
-Person2=$2
+Person1=$1 #This is the first value on the command line 
+Person2=$2 #This is the second value on the command line 
 
 echo "$Person1: Hi $Person2, Good morning"
 echo "$Person2: Good morning $Person1"
