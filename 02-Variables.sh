3fa26e9c6a78497d04af3fd284536fa057127ed7
Naveen921 <donthamnaveen@gmail.com>
2023-08-23 22:08:40 +0530

    variables

---
+++ 02-Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Person1=Naveen
+Person2=Venu
+
+echo "$Person1: Hi $Person2, Good morning"
+echo "$Person2: Good morning $Person1"
+echo "$Person1: How are you $Person2"
+echo "$Person2: I am good, how are you $Person1"
