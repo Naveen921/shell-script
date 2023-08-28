0c344cb307035f23f78d3b3a1dae989c3ab876c2
Naveen921 <donthamnaveen@gmail.com>
2023-08-28 22:04:35 +0530

    Multipleinstall-loops

--- 13-Mutipleinstall-Loops.sh
+++ 13-Mutipleinstall-Loops.sh
@@ -27,15 +27,15 @@ VALIDATE(){
    fi
 }
 
-for i in  $@
-do
-    yum list installed $i &>>$LOGFILE
-    if [ $? -ne 0 ]
-     then
-        echo "$i is not installed,let's install it"
-        yum install $i &>>$LOGFILE
-        VALIDATE $? "$i"
-     else
-        echo -e "$Y $i is already installed $N"
-      fi
+for i in $@
+do 
+   yum list installed $i &>>$LOGFILE
+   if [ $? -ne 0 ]
+   then
+      echo "$i not installed, let's install it"
+      yum install $i -y &>>$LOGFILE
+      VALIDATE $? "$i"
+   else
+      echo -e "$Y $i is already installed $N"
+   fi
 done
